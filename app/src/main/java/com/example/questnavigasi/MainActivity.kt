d9a254bf27382e5d4b92c9dd31bba68afcb5e6a0
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-21 23:50:32 +0700

    perbaikan

--- app/src/main/java/com/example/questnavigasi/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasi/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestNavigasiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaanHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/example/questnavigasi/PengelolaanHalaman.kt
+++ app/src/main/java/com/example/questnavigasi/PengelolaanHalaman.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.questnavigasi.model.DataKelamin
 import com.example.questnavigasi.ui.view.DetailMahasiswaView
