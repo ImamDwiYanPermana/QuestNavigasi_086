1f672c034dbe3ec3f9ada53b5f87c72d998ce082
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-21 23:15:22 +0700

    menambahkan componen untuk input data mahasiswa

--- app/src/main/java/com/example/questnavigasi/ui/view/FormMahasiswaView.kt
+++ app/src/main/java/com/example/questnavigasi/ui/view/FormMahasiswaView.kt
@@ -1,2 +1,22 @@
 package com.example.questnavigasi.ui.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import java.lang.reflect.Modifier
+
+@Composable
+fun FormMahasiswaView(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) -> Unit
+) {
+    var name by rememberSaveable { mutableStateOf("") }
+    var nim by rememberSaveable { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var noHP by remember { mutableStateOf("") }
+    var selectedGender by rememberSaveable { mutableStateOf("") }
+
+    val dataMahasiswa: MutableList<String> = mutableListOf(name, nim, selectedGender, email, alamat, noHP)
