6ced927bacbe16f6c508c962e52eb5cb4a4a5ee4
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-21 23:31:44 +0700

    membuatn bagian Ui nya menggunakan row ( pilih jenis kelamin menggunakan radio button) dan membuat input filed untuk email,alamat,noHP dab membuat tombol simpan

--- app/src/main/java/com/example/questnavigasi/ui/view/FormMahasiswaView.kt
+++ app/src/main/java/com/example/questnavigasi/ui/view/FormMahasiswaView.kt
@@ -4,17 +4,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -47,7 +53,7 @@ fun FormMahasiswaView(
             onValueChange = {name = it},
             placeholder = { Text("Masukkan nama")},
             label = {Text("Nama")},
-            modifier = Modifier.fil().padding(5.dp)
+            modifier = Modifier.fillMaxWidth().padding(5.dp)
         )
 
         OutlinedTextField(
@@ -70,3 +76,37 @@ fun FormMahasiswaView(
                 }
             }
         }
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            placeholder = { Text("Masukkan email")},
+            label = {Text("Email")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth().padding(5.dp)
+        )
+
+        OutlinedTextField(
+            value = alamat,
+            onValueChange = {alamat = it},
+            placeholder = { Text("Masukkan alamat")},
+            label = {Text("Alamat")},
+            modifier = Modifier.fillMaxWidth().padding(5.dp)
+        )
+
+        OutlinedTextField(
+            value = noHP,
+            onValueChange = {noHP = it},
+            placeholder = { Text("Masukkan No Handphone")},
+            label = {Text("No HP")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth().padding(5.dp)
+        )
+
+        Button(onClick = { onSubmitClicked(dataMahasiswa) })
+        {
+            Text("Simpan")
+
+        }
+
+    }
+}
